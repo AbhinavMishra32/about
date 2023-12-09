4407417127a0084114d432a9341be1fa568d7e4f
Abhinav Mishra <abhinavmishra3322@gmail.com>
2023-12-09 22:58:59 +0530

    Commented out router.get('/') in login.js

--- routes/login.js
+++ routes/login.js
@@ -4,10 +4,10 @@ const router = express.Router();
 const path = require('path');
 app.use(express.json());
 
-router.get('/', (req, res) =>{
-    // res.sendFile('public/login/login.html');
-    res.sendFile(path.join(__dirname, '../public/login/login.html'));
-});
+// router.get('/', (req, res) =>{
+//     // res.sendFile('public/login/login.html');
+//     res.sendFile(path.join(__dirname, '../public/login/login.html'));
+// });
 
 router.post('/api/register', async (req, res)=>{
     console.log(req.body);
