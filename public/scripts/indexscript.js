8d3a2f92077163aaa3c51fa73782afacb04a6d26
Abhinav Mishra <abhinavmishra3322@gmail.com>
2023-12-25 16:15:01 +0530

    Remove unnecessary code and update API call in index.html and indexscript.js

--- public/scripts/indexscript.js
+++ public/scripts/indexscript.js
@@ -1,8 +1,23 @@
 document.addEventListener('DOMContentLoaded', async (event) => {
-    try{
-        const result = await fetch('/api/percentage');
+    try{    
+        const result = await fetch('/api/percentage', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const data = await result.json();
+        console.log(data);
+        document.getElementById('percentage').innerHTML = data;
     }
     catch(error){
         console.error('Error: ',error);
     }
-  });
+});
+
+fetch('footer.html')
+.then(response => response.text())
+.then(html => {
+    document.getElementById('footerid').innerHTML = html;
+});
