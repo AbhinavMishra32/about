c0b21016df4fac918261e4b5a56c935cfd5d6c22
AbhinavMishra32 <abhinavmishra3322@gmail.com>
2023-12-27 18:03:31 +0530

    Refactor indexscript.js

--- public/scripts/indexscript.js
+++ public/scripts/indexscript.js
@@ -1,25 +1,23 @@
-document.addEventListener('DOMContentLoaded', async (event) => {
-    try{    
-        const result = await fetch('/api/percentage', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        });
-
-        const data = await result.json();
-        const {percentageTime} = data;
-        const roundedValue = percentageTime.toFixed(2   ); 
-        // console.log(data);  
-        document.getElementById('percentage').innerHTML = roundedValue;
-    }
-    catch(error){
-        console.error('Error: ',error);
-    }
+document.addEventListener("DOMContentLoaded", async (event) => {
+  try {
+    const result = await fetch("/api/percentage", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await result.json();
+    const { percentageTime } = data;
+    const roundedValue = percentageTime.toFixed(2);
+    // console.log(data);
+    document.getElementById("percentage").innerHTML = roundedValue;
+  } catch (error) {
+    console.error("Error: ", error);
+  }
 });
 
-fetch('footer.html')
-.then(response => response.text())
-.then(html => {
-    document.getElementById('footerid').innerHTML = html;
-});
+fetch("footer.html")
+  .then((response) => response.text())
+  .then((html) => {
+    document.getElementById("footerid").innerHTML = html;
+  });
